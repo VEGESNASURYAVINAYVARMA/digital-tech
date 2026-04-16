b92706296817444f56f806d04e96d3d8c76ef5e9
srujan <srujan@gmail.com>
2026-04-16 15:07:13 +0530

    add new version expense project

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Expense
-# Register your models here.
+from .models import Expense, Friend, Group, GroupMember, SplitExpense, SplitShare
+
 admin.site.register(Expense)
+admin.site.register(Friend)
+admin.site.register(Group)
+admin.site.register(GroupMember)
+admin.site.register(SplitExpense)
+admin.site.register(SplitShare)
